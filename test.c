bf02958fa541d25cc866db1318e93400dc825121
bikshan chatterjee <bchatterjee7980@gmail.com>
2022-09-28 01:01:22 +0530

    aaa

--- test.c
+++ test.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "des.h"
 
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    printf("aaa\n");
-    char ch[100];
-    char a;
-    scanf("%c", &a);
-    for(int i=0; i<n; i++)
-    {
-	scanf("%c", &ch[i]);
-    }
-    for(int i=0; i<n; i++)
-    {
-	printf("%d ", ch[i]);
-    }
+    unsigned char* plaintext = malloc(8 * sizeof(unsigned char));
+    unsigned char* ciphertext = malloc(8 * sizeof(unsigned char));
+    unsigned char* decryptedtext = malloc(8 * sizeof(unsigned char));
+    unsigned char* key = malloc(8 * sizeof(unsigned char));
+
+    char message[] = "thikache";
+    char key_str[] = "abcdefgh";
+    char iv_str[] = "ijklmnop";
+
+    for(int i=0; i<8; i++) plaintext[i] = message[i];
+    for(int i=0; i<8; i++) key[i] = key_str[i];
+
+    printf("plaintext:\n");
+    for(int i=0; i<8; i++)
+	printf("%c", plaintext[i]);
+    printf("\n\n");
+
+    des_enc(plaintext, key, ciphertext);
+
+    printf("ciphertext:\n");
+    for(int i=0; i<8; i++)
+	printf("%02x ", ciphertext[i]);
+    printf("\n\n");
+
+    des_dec(ciphertext, key, decryptedtext);
+
+    printf("decryptedtext:\n");
+    for(int i=0; i<8; i++)
+	printf("%c", decryptedtext[i]);
+    printf("\n\n");
+
     return 0;
 }
